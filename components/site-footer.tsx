de3cecfe93cd652941f2183700695a4235ec1c90
LetTTGACO <1225751694@qq.com>
2024-05-10 21:43:23 +0800

    feat: 备案

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,8 +6,11 @@ export function SiteFooter() {
           <div className="mx-auto">
             <hr className="border-b-1 mb-4 border-gray-200" />
             <div className="flex flex-wrap items-center justify-center md:justify-between">
-                <div className="w-full px-4 md:w-4/12">
-                  <div className="mb-2 text-center md:mb-0 md:text-left"><a href="https://1874.cool" target="_blank" className="text-blueGray-500 py-1 text-center text-sm font-semibold md:text-left" rel="noreferrer">Copyright © 2023 1874</a></div>
+                <div className="w-full px-4 ">
+                  <div className="mb-2 flex justify-between text-center md:mb-0 md:text-left">
+                    <a href="https://1874.cool" target="_blank" className="text-blueGray-500 py-1 text-center text-sm font-semibold md:text-left" rel="noreferrer">Copyright © 2023 1874</a>
+                    <a href="https://beian.miit.gov.cn/" target="_blank" className="text-blueGray-500 ml-2 py-1 text-center text-sm font-semibold md:text-left" rel="noreferrer">陕ICP备18013290号-3</a>
+                  </div>
                 </div>
             </div>
           </div>
