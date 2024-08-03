9a35cfc21f32df90f96934aeab450e8ecd9034d7
LetTTGACO <1225751694@qq.com>
2024-08-04 02:06:48 +0800

    feat: Copyright

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -8,7 +8,7 @@ export function SiteFooter() {
             <div className="flex flex-wrap items-center justify-center md:justify-between">
                 <div className="w-full px-4 ">
                   <div className="mb-2 flex justify-between text-center md:mb-0 md:text-left">
-                    <a href="https://1874.cool" target="_blank" className="text-blueGray-500 py-1 text-center text-sm font-semibold md:text-left" rel="noreferrer">Copyright © 2023 1874</a>
+                    <a href="https://1874.cool" target="_blank" className="text-blueGray-500 py-1 text-center text-sm font-semibold md:text-left" rel="noreferrer">Copyright © 2024 1874</a>
                     <a href="https://beian.miit.gov.cn/" target="_blank" className="text-blueGray-500 ml-2 py-1 text-center text-sm font-semibold md:text-left" rel="noreferrer">陕ICP备18013290号-3</a>
                   </div>
                 </div>
